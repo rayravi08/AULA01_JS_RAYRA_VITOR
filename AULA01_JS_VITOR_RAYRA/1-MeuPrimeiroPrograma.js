b677d0d0375ae9294c41c4982eb87aac663e77bc
rayravi08 <vitor.rayra123@gmail.com>
2024-11-13 15:05:41 -0300

    Add files via upload

---
+++ AULA01_JS_VITOR_RAYRA/1-MeuPrimeiroPrograma.js
@@ -0,0 +1,18 @@
+//*primeira instrução em JS
+console.log("Olá mundo cruel da programação\n");
+
+const aluno01 = "Rayra";
+const aluno02 = "Vitor";
+
+const idadeAluno01 = 16;
+const idadeAluno02 = 16;
+
+const somaIdades = idadeAluno01 + idadeAluno02;
+
+const dupla = aluno01 + " e " + aluno02;
+
+console.log("Somos a dupla: " + dupla + "\n");
+console.log("A soma das nossas idades é: " + somaIdades + " anos");
+
+document.write("<h1> Somos a dupla: </h1>" + dupla);
+document.write("A soma das nossas idades é: " + somaIdades + " anos");
